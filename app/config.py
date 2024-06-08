95cd2a6ec394ce740734d79a42b2920909beb41b
dimondimskiy <skdmyr@gmail.com>
2024-06-08 06:10:59 +0200

    scopes, default_limit added

--- app/config.py
+++ app/config.py
@@ -11,3 +11,6 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 # <------ end jwt
+SCOPES = {"admin": {"muscle_group_post", "muscle_post", "muscle_delete"}}
+
+DEFAULT_LIMIT = 20
