3224ba1e0e7d3a65ce98edfdf10188c03a315ccd
dimondimskiy <skdmyr@gmail.com>
2024-06-08 06:13:49 +0200

    added permissions

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,8 @@
 import bcrypt
+from fastapi import HTTPException, status
+
+from app.schemas import UserPublic
+from app.config import SCOPES
 
 
 def get_hashed_pwd(plain_pwd: str) -> bytes:
@@ -10,3 +14,10 @@ def get_hashed_pwd(plain_pwd: str) -> bytes:
 def verify_pwd(plain_pwd: str, hashed_pwd: bytes):
     pwd = plain_pwd.encode("utf-8")
     return bcrypt.checkpw(pwd, hashed_pwd)
+
+
+def check_permission(user: UserPublic, required: str):
+    if required not in SCOPES.get(user.user_type, {}):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Not enough rights"
+        )
